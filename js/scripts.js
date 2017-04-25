c7a854bdf7a576c8f682fc576ead0c2fc3ba7088
Justin Gomez and Joe Parsley <justin@>
2017-04-25 13:55:48 -0700

    add functioning toggle for flashcards

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,26 @@
 $(function() {
-$(".col-md-4").click (function() {
+$("#javascript-show, #javascript-hide").click (function() {
     $("#javascript-show").toggle();
     $("#javascript-hide").toggle();
-
+  });
+$("#operators-show, #operators-hide").click (function() {
+      $("#operators-show").toggle();
+      $("#operators-hide").toggle();
+  });
+$("#variables-show, #variables-hide").click (function() {
+      $("#variables-show").toggle();
+      $("#variables-hide").toggle();
+  });
+$("#vnc-show, #vnc-hide").click (function() {
+      $("#vnc-show").toggle();
+      $("#vnc-hide").toggle();
+  });
+$("#functions-show, #functions-hide").click (function() {
+      $("#functions-show").toggle();
+      $("#functions-hide").toggle();
+  });
+$("#methods-show, #methods-hide").click (function() {
+      $("#methods-show").toggle();
+      $("#methods-hide").toggle();
   });
 });
